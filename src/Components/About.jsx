1eeabcc61742c447bce24d0f2fcfb6c20af25e64
Ademide <alaseademide02@gmail.com>
2025-07-22 16:15:13 +0100

    okay

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function About() {
   return (
-    <div className='mt-12 w-full mx-auto p-5 bg-gray-200/70 rounded-lg shadow-lg md:w-[60%]'>
+    <div className='mt-12 w-[90%] mx-auto p-5 bg-gray-200/70 rounded-lg shadow-lg md:w-[60%]'>
       <h2 className='text-center text-2xl font-semibold text-gray-500'>About Me</h2>
       <p className='mt-2 bg-gray-100/70 p-5 text-center'>
         I'm Alase Samuel, a passionate Frontend Developer with a love for clean UI
--- src/Components/Experience.jsx
+++ src/Components/Experience.jsx
@@ -2,17 +2,17 @@ import React from 'react'
 
 export default function Experience() {
   return (
-    <div  className='mt-12 w-[60%] mx-auto p-10 bg-gray-200/70 rounded-lg shadow-lg'>
+    <div  className='mt-12 w-[90%] mx-auto bg-gray-200/70 rounded-lg shadow-lg'>
       <h2 className='text-2xl font-semibold text-center mb-6'>Experience</h2>
       <ul className='bg-gray-100/70'>
-        <li>
+        <li className='p-5'>
           <h3 className='text-xl font-semibold text-gray-400/90 mt-2'>Frontend Developer</h3>
          <p className="mt-2 text-gray-700 leading-relaxed">
               Built responsive websites, collaborated with designers, and contributed to frontend architecture decisions.
             </p>
 
         </li>
-        <li>
+        <li className='p-5'>
           <h3  className='text-xl font-semibold text-gray-400/90'>Data Analysis</h3>
           <p className="mt-2 text-gray-700 leading-relaxed">
             Working with Excel and Power BI for data visualization Analyzing
