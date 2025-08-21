c19f87cc442b435e9c0214fd29411751f79cd322
Ademide <alaseademide02@gmail.com>
2025-08-21 15:12:14 +0100

    okay

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -31,11 +31,11 @@ export default function Navbar() {
           {/* <div className="w-4 h-0.5 bg-gray-800"></div> */}
         </button>
         <div
-          className={`absolute right-0 border bg-gray-600/45 shadow-md rounded-b-lg p-3 z-15 w-full text-left ${
+          className={`absolute right-0 border bg-gray-600/80 shadow-md rounded-b-lg p-3 z-15 w-full text-left ${
             menuOpen ? "top-16" : "-top-100"
           } transition-all duration-700 md:relative md:z-15 md:top-0 md:bg-inherit md:border-none md:shadow-none md:w-auto`}
         >
-          <ul className="space-y-2 text-sm flex flex-col gap-1 md:gap-5 md:flex-row">
+          <ul className="space-y-2 text-sm flex flex-col gap-1 md:gap-5 md:flex-row text-white">
             <li className=" cursor-pointer " onClick={handleMenuToggle}>
               <Link to="about">About</Link>
             </li>
