c4b06d9bde8e8cfa66845286e432e8c66bd5a1f6
Ademide <alaseademide02@gmail.com>
2025-08-21 09:57:42 +0100

    okay

--- src/Components/Experience.jsx
+++ src/Components/Experience.jsx
@@ -2,28 +2,55 @@ import React from "react";
 
 export default function Experience() {
   return (
-    <div className="mt-12 w-[90%] mx-auto bg-gray-200/70 rounded-lg shadow-lg">
-      <h2 className="text-2xl font-semibold text-center mb-6 underline">Experience</h2>
-      <ul className="bg-gray-100/70 text-center">
-        <li className="p-5">
-          <h3 className="text-xl font-semibold text-gray-400/90 mt-2">
+    <div>
+      <h2 className="text-2xl font-semibold text-center mb-6 underline">
+        Experience
+      </h2>
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-5 mt-12 w-[90%] mx-auto p-5 bg-gray-200/70 rounded-lg shadow-lg md:w-[70%]">
+        <div className=" w-[90%] mx-auto p-5 bg-gray-200/70 rounded-lg shadow-lg md:w-[70%]">
+          <span className="text-xl font-semibold text-gray-400/90 mt-2">
             Frontend Developer
-          </h3>
+          </span>
           <p className="mt-2 text-gray-700 leading-relaxed">
             Built responsive websites, collaborated with designers, and
             contributed to frontend architecture decisions.
           </p>
-        </li>
-        <li className="p-5">
-          <h3 className="text-xl font-semibold text-gray-400/90">
+        </div>
+
+         <div  className=" w-[90%] mx-auto p-5 bg-gray-200/70 rounded-lg shadow-lg md:w-[70%]">
+          <span className="text-xl font-semibold text-gray-400/90 mt-2">
             Data Analysis
-          </h3>
+          </span>
           <p className="mt-2 text-gray-700 leading-relaxed">
-            Working with Excel and Power BI for data visualization Analyzing
-            datasets (e.g., sales, sleep patterns)
+             Working with Excel and Power BI for data visualization Analyzing
+    //         datasets (e.g., sales, sleep patterns)
           </p>
-        </li>
-      </ul>
+        </div>
+      </div>
     </div>
+
+    // <div className="mt-12 w-[90%] mx-auto bg-gray-200/70 rounded-lg shadow-lg">
+    //   <h2 className="text-2xl font-semibold text-center mb-6 underline">Experience</h2>
+    //   <ul className="bg-gray-100/70 text-center">
+    //     <li className="p-5">
+    //       <h3 className="text-xl font-semibold text-gray-400/90 mt-2">
+    //         Frontend Developer
+    //       </h3>
+    //       <p className="mt-2 text-gray-700 leading-relaxed">
+    //         Built responsive websites, collaborated with designers, and
+    //         contributed to frontend architecture decisions.
+    //       </p>
+    //     </li>
+    //     <li className="p-5">
+    //       <h3 className="text-xl font-semibold text-gray-400/90">
+    //         Data Analysis
+    //       </h3>
+    //       <p className="mt-2 text-gray-700 leading-relaxed">
+    //         Working with Excel and Power BI for data visualization Analyzing
+    //         datasets (e.g., sales, sleep patterns)
+    //       </p>
+    //     </li>
+    //   </ul>
+    // </div>
   );
 }
