bbd143ca33b4069a1f57ffa95b48517ecfdbe409
Ademide <alaseademide02@gmail.com>
2025-08-21 18:03:30 +0100

    okay

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -39,7 +39,7 @@ export default function About() {
               I'm Alase Samuel, a passionate Frontend Developer with a love for
               clean UI and seamless user experiences. I enjoy turning complex
               problems into simple, beautiful interfaces using modern tools like
-              React, Tailwind, and TypeScript." speed={50}/>
+              React, Tailwind, and TypeScript." speed={30}/>
            
           </div>
         </div>
