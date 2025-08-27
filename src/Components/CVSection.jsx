37f5add37fcb3b4fedea4154ff3a57a5cd1e99c0
Ademide <alaseademide02@gmail.com>
2025-08-27 12:23:07 +0100

    okay

--- src/Components/CVSection.jsx
+++ src/Components/CVSection.jsx
@@ -5,7 +5,7 @@ export default function CVSection() {
   return (
     <div className="text-center ">
       <a
-        href="img/Alase samuel.pdf"
+        href="img/Alase samue.pdf"
         target="_blank"
         rel="noopener noreferrer"
         className="flex items-center gap-2 text-black px-4  rounded-lg  transition"
